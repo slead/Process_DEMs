05508664d8d8ba01946d5d5622a54ecc79d9482c
Stephen Lead <stephen.lead@gmail.com>
2022-09-19 10:46:36 +1000

    add comments

--- processDems.py
+++ processDems.py
@@ -1,64 +1,79 @@
+''''
+processDEMS.py
+
+This script is designed to automate the process of creating a DEM from
+a zip file downloaded from Geoscience Australia at https://elevation.fsdf.org.au/
+
+The downloaded zip file contains a series of zipped ascii raster files, which must
+be mosaicked into a single seamless raster before processing can occur.
+
+Author: Stephen Lead (Stephen.Lead@transport.nsw.gov.au, 0410 638 348)
+Date: 18 September, 2022
+
+'''
 import os
 import zipfile
 import arcpy
+from arcpy import env
 
-starting_dir_name = r'C:\Users\slead\Downloads'
-starting_zip_name = ''
+starting_dir_name = r'C:\Users\slead\Downloads\Berry' # directory containing the zip file
+out_gdb_name = "Berry.gdb"  # output geodatabase to hold the new mosaicked raster
+raster_name = "DEM"         # the name of the new mosaicked raster
 
 def unzip_all(dir_name):
     os.chdir(dir_name) # change to directory with main zip file
     for item in os.listdir(dir_name):
         if item.endswith(".zip"): 
-            starting_zip_name = os.path.abspath(item)
-            # print("Processing {}".format(starting_zip_name))
-            zip_ref = zipfile.ZipFile(starting_zip_name)
+            zip_file_name = os.path.abspath(item)
+            zip_ref = zipfile.ZipFile(zip_file_name)
             zip_ref.extractall(dir_name)
             zip_ref.close()
 
-def unzip(dir_name, file_name, cleanup):
+def unzip(dir_name, file_name):
     os.chdir(dir_name)
     file_name = os.path.abspath(file_name)
-    # print("Processing {}".format(file_name))
+    # print("Unzipping {}".format(file_name))
     zip_ref = zipfile.ZipFile(file_name)
     zip_ref.extractall(dir_name)
     zip_ref.close()
-    if cleanup:
-        print("Deleting temporary file {}".format(file_name))
-        os.remove(file_name)
-
-def mosaic(images):
-    arcpy.management.MosaicToNewRaster(r"'C:\Users\slead\Downloads\DEM\NSW Government - Spatial Services\DEM\1 Metre\Dubbo201407-LID1-AHD_6466426_55_0002_0002_1m.asc';'C:\Users\slead\Downloads\DEM\NSW Government - Spatial Services\DEM\1 Metre\Dubbo201407-LID1-AHD_6466428_55_0002_0002_1m.asc'", r"C:\Users\slead\Downloads\DEM\Dubbo_20220918.gdb", "Dubbo_small", None, "32_BIT_FLOAT", None, 1, "LAST", "FIRST")
-
-
 
 if __name__ == "__main__":
     # Unzip the main zip file
-    # unzip_all(starting_dir_name)
+    unzip_all(starting_dir_name)
 
     # The result is a nested set of directories, ultimately containing
-    # a series of zip files, so traverse the newly created directories
+    # a series of zip files, so traverse the newly created directories.
+    # working_dir is the name of the deepest nested directory, containing
+    # the ascii rasters to process
     working_dir = ''
     for root, dirs, files in os.walk(starting_dir_name):
         path = root.split(os.sep)
         for file in files:
-            if file.endswith(".zip") and not file == starting_zip_name:
-                # unzip(root, file, False)
+            if file.endswith(".zip"):
+                unzip(root, file)
                 working_dir = root
 
-    from arcpy import env
+    # Find all the *.asc files and mosaic them into a new raster
     env.workspace = working_dir
+    env.overwriteOutput = True
+    os.chdir(working_dir)
+
     files_to_process = []
-    
     for item in os.listdir(working_dir):
         if item.endswith(".asc"):
             files_to_process.append(item)
-
-    print("\nFiles to process: {}".format((";").join(files_to_process)))
+    print("\nInput raster files to process:\n\t{}".format(("\n\t").join(files_to_process)))
     
+    # Check that the output geodatabase exists; create it if not
+    if not arcpy.Exists(os.path.join(starting_dir_name, out_gdb_name)):
+        print("Creating output geodatabase {}".format(out_gdb_name))
+        arcpy.management.CreateFileGDB(starting_dir_name, out_gdb_name)
+
+    print ("Creating new mosaic raster {}\{}\{}".format(starting_dir_name, out_gdb_name, raster_name))
     arcpy.MosaicToNewRaster_management(\
         (";").join(files_to_process), \
-        r"C:\Users\slead\Flood\data\DEM\Dubbo\Dubbo.gdb", \
-        "new_mosaic2", \
+        os.path.join(starting_dir_name, out_gdb_name), \
+        raster_name, \
         'GEOGCS["GCS_GDA_1994",DATUM["D_GDA_1994",SPHEROID["GRS_1980",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]', \
         "32_BIT_FLOAT", \
         None, \
@@ -66,17 +81,5 @@ if __name__ == "__main__":
         "LAST", \
         "FIRST"
     )
-    
-    # arcpy.management.MosaicToNewRaster(\
-    #     r"C:\Users\slead\Flood\data\DEM\Dubbo\Dubbo201407-LID1-AHD_6466426_55_0002_0002_1m.asc;C:\Users\slead\Flood\data\DEM\Dubbo\Dubbo201407-LID1-AHD_6466428_55_0002_0002_1m.asc", \
-    #     r"C:\Users\slead\Flood\data\DEM\Dubbo\Dubbo.gdb", \
-    #     "new_mosaic", \
-    #     'GEOGCS["GCS_GDA_1994",DATUM["D_GDA_1994",SPHEROID["GRS_1980",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]', \
-    #     "32_BIT_FLOAT", \
-    #     None, \
-    #     1,\
-    #     "LAST", \
-    #     "FIRST")
 
-
-print("finished")
+print("\nFinished")
